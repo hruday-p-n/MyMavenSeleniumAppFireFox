8d7bb802f5f40558f8ac907b11ebbc6bfd6ce50e
hruday-p-n <hrudaypn2005@gmail.com>
2026-03-09 09:00:45 +0530

    three website

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,18 +1,36 @@
 package com.example;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WindowType;
 
 public class App {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
 
         WebDriver driver = new FirefoxDriver();
 
-        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
 
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
+        // Open first website
+        driver.get("https://www.saucedemo.com/");
+        Thread.sleep(5000);
+
+        // Open second website
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://automationexercise.com/");
+        Thread.sleep(5000);
+
+        // Open third website
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://practicetestautomation.com/");
+        Thread.sleep(5000);
+
+        // Keep browser open for 10 seconds
+        Thread.sleep(10000);
+
+        driver.quit();
     }
 }
